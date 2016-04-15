dd26a8ad5481bc6d72196858e0b71e58981c15b1
jangho <jhc@dgu.edu>
2016-04-16 00:46:24 +0900

    Refactoring to view the website using a template

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,8 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 
+from django.template.loader import render_to_string
+
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
@@ -15,4 +17,10 @@ class HomePageTest(TestCase):
         response = home_page(request)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
